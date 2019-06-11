0f9858cfd3dec20f81094eaa83101b2f652c39bb
Timothy Johnson <timothy@idioticdev.com>
2019-06-10 19:01:58 -0700

    Cache mtimes in format script

--- scripts/format.mjs
+++ scripts/format.mjs
@@ -4,11 +4,14 @@ import glob from 'tiny-glob'
 import prettier from 'prettier'
 import { promises as fs } from 'fs'
 
-async function format(path) {
+async function format(path, cache) {
   if (path == 'dest/devtools/bundle.js' || path == 'test/public/bundle.js')
     return true
 
   try {
+    const stat = await fs.stat(path)
+    if (stat.mtimeMs == cache[path]) return true
+
     const contents = await fs.readFile(path, { encoding: 'utf8' })
     const formatted = prettier.format(contents, {
       filepath: path,
@@ -19,11 +22,13 @@ async function format(path) {
     })
     if (formatted == contents) {
       console.log(`\x1b[2m${path}\x1b[0m`)
+      cache[path] = stat.mtimeMs
       return true
     }
 
     console.log(path)
     await fs.writeFile(path, formatted)
+    cache[path] = (await fs.stat(path)).mtimeMs
     return true
   } catch (err) {
     console.error(`\x1b[31m${path}\x1b[0m`)
@@ -33,7 +38,16 @@ async function format(path) {
 }
 
 ;(async function() {
+  let cache = {}
+
+  try {
+    cache = JSON.parse(await fs.readFile('.cache', 'utf8'))
+  } catch (err) {}
+
   const files = await glob('!(node_modules)/**/*.{js,mjs,svelte,html,json}')
-  const res = await Promise.all(files.map(format))
+  const res = await Promise.all(files.map(o => format(o, cache)))
+
+  await fs.writeFile('.cache', JSON.stringify(cache))
+
   if (res.some(o => !o)) process.exit(1)
 })()
