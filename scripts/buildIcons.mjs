9d5be1345c9570d33a3039264aa48cdd49ada8a8
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-08-28 19:13:11 +0700

    feat: static icon assets

--- scripts/buildIcons.mjs
+++
@@ -1,8 +0,0 @@
-import svgexport from 'svgexport';
-
-svgexport.render([
-	{
-		input: ['src/svelte-logo.svg'],
-		output: [16, 24, 48, 96, 128].map((size) => [`dest/icon-${size}.png`, `${size}:`]),
-	},
-]);
