18624de87701b16d9e335a1a8f02ce8d8108b690
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-08-28 18:01:14 +0700

    manifest v3

--- static/devtools/index.js
+++ static/devtools/index.js
@@ -3,12 +3,13 @@ chrome.devtools.panels.create(
 	chrome.devtools.panels.themeName === 'dark'
 		? '/devtools/svelte-logo-dark.svg'
 		: '/devtools/svelte-logo-light.svg',
-	'/devtools/panel.html',
-	(panel) =>
+	'/index.html',
+	(panel) => {
 		panel.onShown.addListener(() =>
 			chrome.devtools.inspectedWindow.eval(
 				'if (window.__svelte_devtools_select_element) window.__svelte_devtools_select_element($0)',
-				(result, err) => err && console.error(err),
+				(_, err) => err && console.error(err),
 			),
-		),
+		);
+	},
 );
