a1c07a0af44db439c89f34ba1290d05f853271de
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-08-29 12:36:02 +0700

    lint

--- static/color-scheme.js
+++ static/color-scheme.js
@@ -3,13 +3,3 @@ if (chrome.devtools.panels.themeName === 'dark') {
 } else {
 	document.body.classList.remove('dark');
 }
-
-if (chrome.devtools.panels.onThemeChanged) {
-	chrome.devtools.panels.onThemeChanged.addListener(() => {
-		if (chrome.devtools.panels.themeName === 'dark') {
-			document.body.classList.add('dark');
-		} else {
-			document.body.classList.remove('dark');
-		}
-	});
-}
