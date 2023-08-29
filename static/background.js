a01f351c4c0d03d495ccb0c0860cd03a72f3a494
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-08-29 12:35:51 +0700

    simplify background.js

--- static/background.js
+++ static/background.js
@@ -1,67 +1,58 @@
 const ports = new Map();
 
 chrome.runtime.onConnect.addListener((port) => {
-	if (port.sender.url == chrome.runtime.getURL('/devtools/panel.html')) {
-		port.onMessage.addListener(handleToolsMessage);
+	if (port.sender?.url === chrome.runtime.getURL('/index.html')) {
+		port.onMessage.addListener((message, port) => {
+			// 'init' and 'reload' messages do not need to be delivered to content script
+			switch (message.type) {
+				case 'init': {
+					chrome.tabs.executeScript(message.tabId, {
+						code: message.profilerEnabled
+							? 'window.sessionStorage.SvelteDevToolsProfilerEnabled = "true"'
+							: 'delete window.sessionStorage.SvelteDevToolsProfilerEnabled',
+						runAt: 'document_start',
+					});
+
+					ports.set(message.tabId, port);
+
+					port.onDisconnect.addListener(() => {
+						ports.delete(message.tabId);
+
+						chrome.tabs.onUpdated.removeListener(attach);
+						chrome.tabs.sendMessage(message.tabId, {
+							// Inform content script that it needs to clean up
+							type: 'clear',
+							tabId: message.tabId,
+						});
+					});
+
+					return chrome.tabs.onUpdated.addListener(attach);
+
+					/** @type {Parameters<chrome.tabs.TabUpdatedEvent['addListener']>[0]} */
+					function attach(tabId, changed) {
+						const firefox = !window.chrome && !changed.url;
+						if (!ports.has(tabId) || changed.status !== 'loading' || firefox) return;
+						chrome.tabs.executeScript(tabId, {
+							file: '/privilegedContent.js',
+							runAt: 'document_start',
+						});
+					}
+				}
+				case 'reload':
+					return chrome.tabs.reload(message.tabId, { bypassCache: true });
+				default:
+					return chrome.tabs.sendMessage(message.tabId, message);
+			}
+		});
 	} else {
 		// This is not an expected connection, so we just log an error and close it
-		console.error('Unexpected connection. Port ', port);
+		console.error(`Unexpected connection. Port ${port}`);
 		port.disconnect();
 	}
 });
 
-function handleToolsMessage(msg, port) {
-	switch (msg.type) {
-		// 'init' and 'reload' messages do not need to be delivered to content script
-		case 'init':
-			setup(msg.tabId, port, msg.profilerEnabled);
-			break;
-		case 'reload':
-			chrome.tabs.reload(msg.tabId, { bypassCache: true });
-			break;
-		default:
-			chrome.tabs.sendMessage(msg.tabId, msg);
-			break;
-	}
-}
-
 // Receive messages from content scripts
-chrome.runtime.onMessage.addListener((msg, sender) => handlePageMessage(msg, sender.tab.id));
-
-function handlePageMessage(msg, tabId) {
-	const tools = ports.get(tabId);
-	if (tools) tools.postMessage(msg);
-}
-
-function attachScript(tabId, changed) {
-	const chromium = typeof window !== 'undefined' && window.chrome;
-	if (chromium && (!ports.has(tabId) || changed.status != 'loading')) {
-		chrome.tabs.executeScript(tabId, {
-			file: '/privilegedContent.js',
-			runAt: 'document_start',
-		});
-	}
-}
-
-function setup(tabId, port, profilerEnabled) {
-	chrome.tabs.executeScript(tabId, {
-		code: profilerEnabled
-			? `window.sessionStorage.SvelteDevToolsProfilerEnabled = "true"`
-			: 'delete window.sessionStorage.SvelteDevToolsProfilerEnabled',
-		runAt: 'document_start',
-	});
-
-	ports.set(tabId, port);
-
-	port.onDisconnect.addListener(() => {
-		ports.delete(tabId);
-		chrome.tabs.onUpdated.removeListener(attachScript);
-		// Inform content script that it background closed and it needs to clean up
-		chrome.tabs.sendMessage(tabId, {
-			type: 'clear',
-			tabId: tabId,
-		});
-	});
-
-	chrome.tabs.onUpdated.addListener(attachScript);
-}
+chrome.runtime.onMessage.addListener((message, sender) => {
+	const tools = ports.get(sender.tab?.id);
+	if (tools) tools.postMessage(message);
+});
