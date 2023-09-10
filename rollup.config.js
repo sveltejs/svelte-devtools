7d505c310dbaf10ea2ab3914a2e5dfb24ddd5360
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-09-10 19:12:32 +0700

    simulate hmr

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,18 @@
 
 import { defineConfig } from 'rollup';
 
-export default defineConfig({
-	input: 'src/client/index.js',
-	output: [
-		{ file: 'static/courier.js', format: 'iife' },
-		{ file: 'build/courier.js', format: 'iife' },
-	],
-});
+export default defineConfig([
+	{
+		input: 'static/background.js',
+		output: {
+			file: 'build/background.js',
+		},
+	},
+	{
+		input: 'src/client/index.js',
+		output: [
+			{ file: 'static/courier.js', format: 'iife' },
+			{ file: 'build/courier.js', format: 'iife' },
+		],
+	},
+]);
