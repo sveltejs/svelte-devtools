02425483a8b51e40b93ee1fbb53823ba1d5b9b9c
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-10-06 09:26:07 +0700

    chore: remove obsolete test directory (#150)

--- test/src/Events/EventsComponent.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { createEventDispatcher } from 'svelte';
-
-	const dispatch = createEventDispatcher();
-	const handler = () => dispatch('click', 'I was clicked!');
-</script>
-
-<button type="button" on:click|once|stopPropagation|preventDefault={handler}> Click me! </button>
--- test/src/Slots/Slots.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import SlotComponent from './SlotComponent.svelte';
-</script>
-
-<div>
-	<p>Render slots.</p>
-	<SlotComponent><span>Slot content</span></SlotComponent>
-</div>
